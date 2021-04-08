52f15a607fa3c7bcf40a63aa9417c34b1937bc65
Richard <richard.yang.tong@gmail.com>
2021-04-08 06:36:19 -0400

    Treated input2 the same way.

--- using-ts.ts
+++ using-ts.ts
@@ -1,6 +1,6 @@
 const button = document.querySelector('button');
 const input1 = document.getElementById('num1')! as HTMLInputElement;
-const input2 = document.getElementById('num2');
+const input2 = document.getElementById('num2')! as HTMLInputElement;
 
 const add = (num1, num2) => {
     return num1 + num2;
